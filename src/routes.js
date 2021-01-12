e3848b6bdd50c139e56951856e6787bc6f8afd8c
Jake Dibble <jdibble@bsu.edu>
2021-01-11 20:54:17 -0500

    Minor console.log edits

--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/", function(req,res){
-    console.log("--Index loaded--");
+    console.log("--Index requested");
     res.render("index");
+    console.log("--Index loaded");
 });
 
 router.get("/Help",function (req,res){
-    console.log("--Help page loaded--");
+    console.log("--Help page requested");
     res.render("Pages/Help");
+    console.log("--Help page loaded");
 });
 
 router.get("/CreateAccount",function (req,res){
-    console.log("--CreateAccount page loaded--");
+    console.log("--CreateAccount page requested");
     res.render("Pages/CreateAccount");
+    console.log("--CreateAccount page loaded");
 });
 
 
